30a88ab6db039b8efac809527296c82899ff3527
Zan Javed <zjaved101@gmail.com>
2018-10-03 14:27:23 -0700

    Added basic searchbar

--- packages/frontend/src/components/home.jsx
+++ packages/frontend/src/components/home.jsx
@@ -1,9 +1,41 @@
 // @flow
 import '../styles/global.css';
-import React from 'react';
+import React, { Component } from 'react';
 import type { Node } from 'react';
 import { hot } from 'react-hot-loader';
 
-const Home = (): Node => <h1>HOME PAGE</h1>;
+class SearchBar extends Component {
+  state = {
+    query: '',
+  }
+ 
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    })
+  }
+ 
+  render() {
+    return (
+      <div>
+        <h1>HOME PAGE</h1>
+        <form>
+          <input
+            placeholder="Search for event..."
+            ref={input => this.search = input}
+            onChange={this.handleInputChange}
+          />
+          <p>{this.state.query}</p>
+        </form>
+      </div>
+      
+    )
+  }
+}
 
-export default hot(module)(Home);
+// const Home = (): Node => (
+//   <h1>HOME PAGE</h1>
+// );
+
+
+export default hot(module)(SearchBar);
